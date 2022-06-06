44e9150dddc9f130fc71654d7e8f211acdc3a6ae
Eliel Ardon <cuentaroja11@gmail.com>
2022-06-06 17:23:18 -0600

    Cambios iniciales

--- 01_reflex_agent.js
+++ 01_reflex_agent.js
@@ -1,5 +1,7 @@
 // MIT License
 // Copyright (c) 2020 Luis Espino
+// MIT License
+// Copyright (c) 2022 201503986 Edwar Everaldo Zacarias
 
 function reflex_agent(location, state){
     if (state=="DIRTY") return "CLEAN";
@@ -11,13 +13,17 @@ function test(states){
        var location = states[0];		
        var state = states[0] == "A" ? states[1] : states[2];
        var action_result = reflex_agent(location, state);
-       document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
+       document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result + " ").concat(states[1]).concat(" ").concat(states[2]);
        if (action_result == "CLEAN"){
          if (location == "A") states[1] = "CLEAN";
           else if (location == "B") states[2] = "CLEAN";
        }
        else if (action_result == "RIGHT") states[0] = "B";
-       else if (action_result == "LEFT") states[0] = "A";		
+       else if (action_result == "LEFT") states[0] = "A";
+       
+       if (location == "A" && states[1] == "CLEAN" && states[2] == "CLEAN"){
+
+       }
  setTimeout(function(){ test(states); }, 2000);
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# IA_2022JUNIO
+# IA_2022JUNIO 201503986 Edwar Everaldo Zacarias
+Repositorio de ejemplos para el curso de Inteligencia Artificial 1. Junio de 2022
+
+[01 Reflex Agent](https://cuentaroja11.github.io/IA_2022JUNIO/01_relfex_agent.html)
