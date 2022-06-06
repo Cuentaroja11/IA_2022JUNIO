e5265bcefe278678a571082bfad0b421162fbd36
Eliel Ardon <cuentaroja11@gmail.com>
2022-06-06 17:32:56 -0600

    Detener

--- 01_reflex_agent.js
+++ 01_reflex_agent.js
@@ -5,7 +5,7 @@
 
 function reflex_agent(location, state){
     if (state=="DIRTY") return "CLEAN";
-    else if (location=="A") return "RIGHT";
+    else if (location=="A") return "RIGT";
     else if (location=="B") return "LEFT";
 }
 
@@ -13,18 +13,18 @@ function test(states){
        var location = states[0];		
        var state = states[0] == "A" ? states[1] : states[2];
        var action_result = reflex_agent(location, state);
-       document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result + " ").concat(states[1]).concat(" ").concat(states[2]);
+       document.getElementById("log").innerHTML+="<br>Actual: ".concat(location+" | Action: "+action_result+" | "+states[1]+" | "+states[2]);
        if (action_result == "CLEAN"){
          if (location == "A") states[1] = "CLEAN";
           else if (location == "B") states[2] = "CLEAN";
        }
-       else if (action_result == "RIGHT") states[0] = "B";
+       else if (action_result == "RIGT") states[0] = "B";
        else if (action_result == "LEFT") states[0] = "A";
        
-       if (location == "A" && states[1] == "CLEAN" && states[2] == "CLEAN"){
-
+       if (!(location == "A" && states[1] == "CLEAN" && states[2] == "CLEAN")){
+        setTimeout(function(){ test(states); }, 1000);
        }
- setTimeout(function(){ test(states); }, 2000);
+ 
 }
 
 var states = ["A","DIRTY","DIRTY"];
