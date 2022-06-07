42966eed2f189e141b7ed096104d29c163f5ad4d
Eliel Ardon <cuentaroja11@gmail.com>
2022-06-06 18:20:46 -0600

    Verificar estados

--- 01_reflex_agent.js
+++ 01_reflex_agent.js
@@ -3,6 +3,8 @@
 // MIT License
 // Copyright (c) 2022 201503986 Edwar Everaldo Zacarias
 
+var vuelta = 0;
+
 function reflex_agent(location, state){
     if (state=="DIRTY") return "CLEAN";
     else if (location=="A") return "RIGT";
@@ -10,21 +12,31 @@ function reflex_agent(location, state){
 }
 
 function test(states){
-       var location = states[0];		
-       var state = states[0] == "A" ? states[1] : states[2];
-       var action_result = reflex_agent(location, state);
-       document.getElementById("log").innerHTML+="<br>Actual: ".concat(location+" | Action: "+action_result+" | "+states[1]+" | "+states[2]);
-       if (action_result == "CLEAN"){
-         if (location == "A") states[1] = "CLEAN";
-          else if (location == "B") states[2] = "CLEAN";
-       }
-       else if (action_result == "RIGT") states[0] = "B";
-       else if (action_result == "LEFT") states[0] = "A";
-       
-       if (!( (states[0] == "A") && (states[1] == "CLEAN") && (states[2] == "CLEAN") )){
-        setTimeout(function(){ test(states); }, 1000);
-       }
- 
+    var location = states[0];		
+    var state = states[0] == "A" ? states[1] : states[2];
+    var action_result = reflex_agent(location, state);
+    document.getElementById("log").innerHTML+="<br>Actual: ".concat(location+" | Action: "+action_result+" | "+states[1]+" | "+states[2]);
+    if (action_result == "CLEAN"){
+        if (location == "A") states[1] = "CLEAN";
+        else if (location == "B") states[2] = "CLEAN";
+    }
+    else if (action_result == "RIGT") states[0] = "B";
+    else if (action_result == "LEFT") states[0] = "A";
+    
+    if (vuelta < 2){
+        if (!( (states[1] == "CLEAN") && (states[2] == "CLEAN") )){
+            setTimeout(function(){ test(states); }, 1000);
+        }
+
+        if (( (states[1] == "CLEAN") && (states[2] == "CLEAN") )){
+            document.getElementById("log").innerHTML+="<br>Actual: ".concat(location+" | Action: "+action_result+" | "+states[1]+" | "+states[2]);
+            states[1] = "DIRTY";
+            states[2] = "DIRTY";
+            vuelta ++;
+            setTimeout(function(){ test(states); }, 1000);
+        }
+        
+    }     
 }
 
 var states = ["A","DIRTY","DIRTY"];
