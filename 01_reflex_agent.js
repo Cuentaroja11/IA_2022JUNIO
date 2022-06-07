c2d44cedc4b2a751ea3823c8ab73093694bf6599
Eliel Ardon <cuentaroja11@gmail.com>
2022-06-06 18:27:44 -0600

    Se modifico el estado

--- 01_reflex_agent.js
+++ 01_reflex_agent.js
@@ -3,15 +3,13 @@
 // MIT License
 // Copyright (c) 2022 201503986 Edwar Everaldo Zacarias
 
-var vuelta = 0;
-
 function reflex_agent(location, state){
     if (state=="DIRTY") return "CLEAN";
     else if (location=="A") return "RIGT";
     else if (location=="B") return "LEFT";
 }
 
-function test(states){
+function test(states,vuelta){
     var location = states[0];		
     var state = states[0] == "A" ? states[1] : states[2];
     var action_result = reflex_agent(location, state);
@@ -23,9 +21,9 @@ function test(states){
     else if (action_result == "RIGT") states[0] = "B";
     else if (action_result == "LEFT") states[0] = "A";
     
-    if (vuelta < 3){
+    if (vuelta < 2){
         if (!( (states[1] == "CLEAN") && (states[2] == "CLEAN") )){
-            setTimeout(function(){ test(states); }, 1000);
+            setTimeout(function(){ test(states,vuelta); }, 1000);
         }
 
         if (( (states[1] == "CLEAN") && (states[2] == "CLEAN") )){
@@ -33,11 +31,11 @@ function test(states){
             states[1] = "DIRTY";
             states[2] = "DIRTY";
             vuelta ++;
-            setTimeout(function(){ test(states); }, 1000);
+            setTimeout(function(){ test(states,vuelta); }, 1000);
         }
         
     }     
 }
 
 var states = ["A","DIRTY","DIRTY"];
-test(states);
+test(states,0);
